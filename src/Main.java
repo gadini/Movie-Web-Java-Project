3ccb0b2b02f6cad7b35aece8eb7466429f48a7d0
gustur0 <alvesrochagadini@gmail.com>
2024-03-04 15:55:38 -0300

    Using Array List

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,54 @@
+import br.com.gadini.github.movieweb.calculos.CalculadoraTempo;
+import br.com.gadini.github.movieweb.calculos.FiltroRecomendacao;
+import br.com.gadini.github.movieweb.modelos.Episodio;
+import br.com.gadini.github.movieweb.modelos.Filme;
+import br.com.gadini.github.movieweb.modelos.Serie;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is the Movie Web");
-        System.out.println("Movie: Top Gun Maverick");
+
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoLancamento(1970);
+        meuFilme.setDuracaoMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoMinutos());
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosTemporada(10);
+        lost.setMinutosEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoMinutos());
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoLancamento(2023);
+        outroFilme.setDuracaoMinutos(200);
+
+        CalculadoraTempo calculadora = new CalculadoraTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
+
+
     }
 }
